16f22b622981674ce749efce745cae1c6680d08f
danisensi7-blip <danisensi7@gmail.com>
2026-08-16 22:20:39 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+console.log("🤖 Danibot iniciado correctamente");
+
+console.log(`
+╔══════════════════════╗
+║      🤖 DANIBOT      ║
+╠══════════════════════╣
+║ .menu                ║
+║ .ping                ║
+║ .hola                ║
+╚══════════════════════╝
+`);
